07ec0430d7f4b32fbf8018bd4c67fbb9a69ffec7
atefrice <atefrice@126.com>
2019-07-29 08:50:23 +0800

    update go dir structure

---
+++ daily_practice/loop_creat_file.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+)
+
+var RoutingCnt int;
+
+func init() {
+	flag.IntVar(&RoutingCnt, "cnt", 20, "Create go routing count.(def: 20)");
+}
+
+
+func main() { 
+
+flag.Parse()
+fmt.Println(RoutingCnt)
+
+
+
+}
